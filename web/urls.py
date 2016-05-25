aee7aae0b8101fa94a98de7c1c819aa763b0d760
LukasPJ <ljorgensen@winterleafentertainment.com>
2016-05-26 01:50:36 +0200

    Implemented personalized ical feeds

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from web.views import Index, Contact, BartenderList, Barplan, Items, Board, Search
+from web.views import Index, Contact, BartenderList, Barplan, UserBarplan, Items, Board, Search
 
 urlpatterns = [
     url(r'^contact/', Contact.as_view()),
     url(r'^bartenders/', BartenderList.as_view()),
+    url(r'^barplan/(?P<username>[0-9a-zA-Z]+)/', UserBarplan()),
     url(r'^barplan/', Barplan.as_view()),
     url(r'^prices/', Items.as_view()),
     url(r'^board/', Board.as_view()),
