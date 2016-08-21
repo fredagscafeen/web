723a4f421818dd6344b5e6b3aa8fca51aafe963d
Alexandra <alexandra.hou@gmail.com>
2016-08-21 21:30:28 +0200

    now implemented functionality, models and view for udlejning grill. Made a dropdown in the menu. Still lacks prettyness.

--- udlejning/admin.py
+++ udlejning/admin.py
@@ -1,8 +1,15 @@
 from udlejning.models import Udlejning
+from udlejning.models import UdlejningGrill
 from django.contrib import admin
 
 
 class UdlejningAdmin(admin.ModelAdmin):
     list_display = ('dateFrom', 'whoReserved', 'boardMemberInCharge')
 
+
+class UdlejningGrillAdmin(admin.ModelAdmin):
+    list_display = ('dateFrom', 'whoReserved', 'boardMemberInCharge')
+
+
 admin.site.register(Udlejning, UdlejningAdmin)
+admin.site.register(UdlejningGrill, UdlejningGrillAdmin)
