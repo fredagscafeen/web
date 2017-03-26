bc872f821cbe4cd771886998fdc215e36dd02bdd
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2017-03-26 21:03:24 +0200

    Add some tests for BartenderApplication sending

--- bartenders/tests.py
+++ bartenders/tests.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.forms import model_to_dict
 from django.test import TestCase
 from django.core import mail
+from rest_framework import status
 
 from bartenders.models import BartenderApplication, Bartender
 
@@ -19,6 +20,7 @@ class BartenderApplicationTests(TestCase):
 		b = model_to_dict(ap)
 		b.update(isActiveBartender=True)
 		b.update(isBoardMember=False)
+		b.pop('info')
 		self.assertDictEqual(b, model_to_dict(Bartender.objects.first()))
 
 		# Test that an email was sent
@@ -27,3 +29,35 @@ class BartenderApplicationTests(TestCase):
 
 		link = urljoin(settings.SELF_URL, reverse('barplan'))
 		self.assertIn(link, mail.outbox[0].body)
+
+	def test_sending_application(self):
+		data = dict(
+			name='Kat',
+			username='Abe',
+			email='abe@cs.au.dk',
+			studentNumber=123123,
+			phoneNumber=12312312,
+			info='Hkll'
+		)
+
+		response = self.client.post('/', data=data)
+		self.assertRedirects(response, '/')
+
+		# Test that application was made
+		self.assertTrue(BartenderApplication.objects.exists())
+		# And mail was sent
+		self.assertEqual(len(mail.outbox), 1)
+
+	def test_invalid_application(self):
+		data = dict(
+			name='Morten',
+			username='Olsen',
+			email='cat@cs.au.no',
+			studentNumber='NaN',
+			phoneNumber=123,
+			info='Kill dogs'
+		)
+
+		response = self.client.post('/', data=data)
+		self.assertFalse(BartenderApplication.objects.exists())
+		self.assertEqual(len(mail.outbox), 0)
