f1bc04dd4c09f55f667edca039da65328f03526b
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2017-03-26 16:13:02 +0200

    Create the BartenderApplication model and admin interface

--- fredagscafeen/settings/base.py
+++ fredagscafeen/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_object_actions',
     'items',
     'bartenders',
     'rest_framework',
