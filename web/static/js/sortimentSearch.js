873e5c79b762da5f10c8da0a201ccbffc20ee4d4
Jonas Tranberg <jonas_ts@msn.com>
2017-04-20 01:37:19 +0200

    sortiment search now with count!

--- web/static/js/sortimentSearch.js
+++ web/static/js/sortimentSearch.js
@@ -5,8 +5,14 @@ $(document).ready(function () {
 
 	$("#itemsSearchInput").on("keyup", function() {
 		var value = $(this).val();
+		var rows = $("table tr");
 
-		$("table tr").each(function(index) {
+		if (rows.length == 1)
+			return;
+
+		var count = 0;
+
+		rows.each(function(index) {
 			if (index !== 0) {
 
 				$row = $(this);
@@ -15,10 +21,16 @@ $(document).ready(function () {
 
 				if (id.toUpperCase().indexOf(value.toUpperCase()) !== -1) {
 					$row.show();
+					count++;
 				}
 				else {
 					$row.hide();
 				}
+
+				if(index == (rows.length - 1) && value != "")
+					$("#sortiment-count").text("(" + count + "/" + (rows.length - 1) + ")");
+				else
+					$("#sortiment-count").text("(" + (rows.length - 1) + ")");
 			}
 		});
 	});
