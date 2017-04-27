48823fc7b8b8ba67860ba40860e2314275363afb
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2017-04-27 05:47:39 +0200

    Add paid-checkmark to list-display of rentals

--- udlejning/admin.py
+++ udlejning/admin.py
@@ -3,8 +3,9 @@ from udlejning.models import UdlejningGrill
 from django.contrib import admin
 
 
+# Remember to cut down to 2 classes
 class UdlejningAdmin(admin.ModelAdmin):
-    list_display = ('dateFrom', 'whoReserved', 'in_charge')
+    list_display = ('dateFrom', 'whoReserved', 'in_charge', 'paid')
     filter_horizontal = ('bartendersInCharge', )
 
     def get_queryset(self, request):
@@ -15,5 +16,17 @@ class UdlejningAdmin(admin.ModelAdmin):
         return ', '.join(obj.bartendersInCharge.values_list('username', flat=True))
 
 
+class UdlejningGrillAdmin(admin.ModelAdmin):
+    list_display = ('dateFrom', 'whoReserved', 'in_charge')
+    filter_horizontal = ('bartendersInCharge',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('bartendersInCharge')
+
+    def in_charge(self, obj):
+        return ', '.join(obj.bartendersInCharge.values_list('username', flat=True))
+
+
 admin.site.register(Udlejning, UdlejningAdmin)
-admin.site.register(UdlejningGrill, UdlejningAdmin)
+admin.site.register(UdlejningGrill, UdlejningGrillAdmin)
