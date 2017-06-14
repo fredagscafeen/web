672054690bf447be1f14a17c00b6cb4785231f54
Jonas Tranberg <jonas_ts@msn.com>
2017-06-14 19:04:45 +0200

    Fixed api auth + login and get token

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, parsers, renderers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
 from api.serializers import BartenderSerializer
 from api.serializers import BeerTypeSerializer
@@ -12,27 +16,42 @@ from items.models import BeerType
 from items.models import Brewery
 from items.models import Item
 
-
 class ItemViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly,)
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication,)
+    
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 
 class BreweryViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly,)
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication,)
+    
     queryset = Brewery.objects.all()
     serializer_class = BrewerySerializer
 
 
 class BeerTypeViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly,)
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication,)
+    
     queryset = BeerType.objects.all()
     serializer_class = BeerTypeSerializer
 
 
 class BartenderViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly,)
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication,)
+    
     queryset = Bartender.objects.all()
     serializer_class = BartenderSerializer
 
+
 class LastModifiedView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication,)
+
     def get(self, request, format=None):
         last_modified = None
         for item in Item.objects.all():
@@ -40,7 +59,11 @@ class LastModifiedView(APIView):
                 last_modified = item.lastModified
         return Response(last_modified, status=status.HTTP_200_OK)
 
+
 class IsBartenderView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication,)
+
     def get(self, request, username, format=None):
         is_bartender = False
         try:
@@ -49,3 +72,19 @@ class IsBartenderView(APIView):
         except Bartender.DoesNotExist:
             pass
         return Response(is_bartender, status=status.HTTP_200_OK)
+
+
+class TokenAuthView(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser)
+    renderer_classes = (renderers.JSONRenderer)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+        
