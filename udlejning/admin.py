0c536e654ffc94baa3aee64bb154a81e65207e8d
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2017-09-18 11:05:31 +0200

    Fix #18: Let Nygaard save one click each time he visits the admin interface

--- udlejning/admin.py
+++ udlejning/admin.py
@@ -5,27 +5,29 @@ from django.contrib import admin
 
 # Remember to cut down to 2 classes
 class UdlejningAdmin(admin.ModelAdmin):
-    list_display = ('dateFrom', 'whoReserved', 'in_charge', 'paid')
-    filter_horizontal = ('bartendersInCharge', )
+	ordering = ('-dateFrom',)
+	list_display = ('dateFrom', 'whoReserved', 'in_charge', 'paid')
+	filter_horizontal = ('bartendersInCharge', )
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.prefetch_related('bartendersInCharge')
+	def get_queryset(self, request):
+		qs = super().get_queryset(request)
+		return qs.prefetch_related('bartendersInCharge')
 
-    def in_charge(self, obj):
-        return ', '.join(obj.bartendersInCharge.values_list('username', flat=True))
+	def in_charge(self, obj):
+		return ', '.join(obj.bartendersInCharge.values_list('username', flat=True))
 
 
 class UdlejningGrillAdmin(admin.ModelAdmin):
-    list_display = ('dateFrom', 'whoReserved', 'in_charge')
-    filter_horizontal = ('bartendersInCharge',)
+	ordering = ('-dateFrom',)
+	list_display = ('dateFrom', 'whoReserved', 'in_charge')
+	filter_horizontal = ('bartendersInCharge',)
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.prefetch_related('bartendersInCharge')
+	def get_queryset(self, request):
+		qs = super().get_queryset(request)
+		return qs.prefetch_related('bartendersInCharge')
 
-    def in_charge(self, obj):
-        return ', '.join(obj.bartendersInCharge.values_list('username', flat=True))
+	def in_charge(self, obj):
+		return ', '.join(obj.bartendersInCharge.values_list('username', flat=True))
 
 
 admin.site.register(Udlejning, UdlejningAdmin)
