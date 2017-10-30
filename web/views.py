7276d7b333f9bebb020b534de10a62bf1dba38be
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2017-10-30 11:12:50 +0100

    Finalize UI

--- web/views.py
+++ web/views.py
@@ -31,13 +31,6 @@ class Index(CreateView):
         return response
 
 
-class NewTestIndex(Index):
-    def get_context_data(self, **kwargs):
-        d =  super().get_context_data(**kwargs)
-        d['test'] = True
-        return d
-
-
 class Contact(TemplateView):
     template_name = "contact.html"
 
