5bfeedab720f6867cc58d8e4c83fbc35bee9a796
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-03-14 03:10:45 +0100

    Upgrade to django 1.11 and upgrade other dependencies

--- api/serializers.py
+++ api/serializers.py
@@ -7,20 +7,24 @@ from items.models import BeerType, Item, Brewery
 class BrewerySerializer(serializers.ModelSerializer):
     class Meta:
         model = Brewery
+        fields = '__all__'
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
 
 
 class BartenderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bartender
         fields = ('id', 'name', 'username', 'isActiveBartender')
+        fields = '__all__'
 
 
 class BeerTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = BeerType
+        fields = '__all__'
 
