59465024693570e95334f69a6d3c33a279bec489
Tyilo <asgerdrewsen@gmail.com>
2018-03-18 19:35:12 +0100

    Generate ICalFeeds from shift objects

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
+from django.urls import path, reverse
+from django.views.generic.base import RedirectView
 
 from web.views import Index, Contact, BartenderList, Barplan, UserBarplan, Items, Board, Search, Udlejninger, \
-    UdlejningerGrill, Guides
+    UdlejningerGrill, Guides, UserDepositShifts
 
 urlpatterns = [
     path('contact/', Contact.as_view()),
     path('bartenders/', BartenderList.as_view()),
     path('barplan/', Barplan.as_view(), name='barplan'),
-    path('barplan/<username>/', UserBarplan()),
+    path('barplan/shifts.ics', UserBarplan()),
+    path('barplan/shifts/<username>.ics', UserBarplan(), name='user_shifts'),
+    path('barplan/deposit_shifts.ics', UserDepositShifts()),
+    path('barplan/deposit_shifts/<username>.ics', UserDepositShifts()),
+    path('barplan/<username>/', RedirectView.as_view(pattern_name='user_shifts')), # Old URLs, redirect
     path('prices/', Items.as_view()),
     path('board/', Board.as_view()),
     path('search/', Search.as_view()),
