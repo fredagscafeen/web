e9bedff5290a2c5e57d284abb6eb427821767377
Tyilo <asgerdrewsen@gmail.com>
2018-03-19 02:46:30 +0100

    Improve reminder commands

--- reminder/management/commands/send_barshift_reminder.py
+++ reminder/management/commands/send_barshift_reminder.py
@@ -1,7 +1,5 @@
 import datetime
 from django.utils import timezone
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 
 from bartenders.models import BartenderShift
 from reminder.management.commands._private import ReminderCommand
@@ -14,37 +12,23 @@ class Command(ReminderCommand):
         return BartenderShift.objects.filter(end_datetime__gte=timezone.now(),
                                              end_datetime__lte=timezone.now() + datetime.timedelta(7)).first()
 
-    def get_bartender_usernames_from_event(self, event):
-        return [b.username for b in event.all_bartenders()]
+    def get_bartenders_from_event(self, event):
+        return event.all_bartenders()
 
-    def send_reminder_email(self, bartenders):
-        subject = 'You have a barshift this friday!'
-        humanize_bartenders = self.humanize_bartenders(bartenders)
+    def email_subject(self, humanized_bartenders):
+        return 'You have a barshift this friday!'
 
-        context = {'content':
-                       'Hello ' + humanize_bartenders + '.\n' \
-                       + '\n' \
-                         'The coming Friday it is YOUR turn to run Fredagscafeen.\n' \
-                         'This is an automated mail sent to you as well as to Bestyrelsen.\n' \
-                         'It is primarily sent so that you may arrange for someone else to take your shift, ' \
-                         'but it also serves as a kind reminder :)\n' \
-                         'Remember that your shift starts at 14:30.\n' \
-                         '\n' \
-                         'See you at the bar!\n' \
-                         '\n' \
-                         '/Bestyrelsen'}
+    def email_body(self, humanized_bartenders):
+        return f'''Hello {humanized_bartenders}.
 
-        body_text = render_to_string('email.txt', context)
-        body_html = render_to_string('email.html', context)
-        recipient_list = self.filter_with_warning(bartenders)
+The coming Friday it is YOUR turn to run Fredagscafeen.
+This is an automated mail sent to you as well as to Bestyrelsen.
+It is primarily sent so that you may arrange for someone else to take your shift, but it also serves as a kind reminder :)
+Remember that your shift starts at 14:30.
 
-        email = EmailMultiAlternatives(
-            subject=subject,
-            body=body_text,
-            from_email='best@fredagscafeen.dk',
-            to=recipient_list,
-            cc=['best@fredagscafeen.dk'],
-            reply_to=['alle@fredagscafeen.dk'])
-        email.attach_alternative(body_html, 'text/html')
-        email.send()
-        print('Reminders sent to ' + humanize_bartenders + '!')
+See you at the bar!
+
+/Bestyrelsen'''
+
+    def email_reply_to(self):
+        return ['alle@fredagscafeen.dk']
--- reminder/management/commands/send_pantvagt_reminder.py
+++ reminder/management/commands/send_pantvagt_reminder.py
@@ -1,7 +1,5 @@
 import datetime
 from django.utils import timezone
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 
 from bartenders.models import BoardMemberDepositShift
 from reminder.management.commands._private import ReminderCommand
@@ -14,31 +12,18 @@ class Command(ReminderCommand):
         return BoardMemberDepositShift.objects.filter(end_date__gte=timezone.now(),
                                                       end_date__lte=timezone.now() + datetime.timedelta(7)).first()
 
-    def get_bartender_usernames_from_event(self, event):
-        return [b.username for b in event.responsibles.all()]
+    def get_bartenders_from_event(self, event):
+        return event.responsibles.all()
 
-    def send_reminder_email(self, bartenders):
-        humanize_bartenders = self.humanize_bartenders(bartenders)
+    def email_subject(self, humanized_bartenders):
+        return f'Hello {humanized_bartenders}. You have to take care of the pant this week!'
 
-        subject = 'Hello ' + humanize_bartenders + '. You have to take care of the pant this week!'
-        context = {'content':
-                       'Hello ' + humanize_bartenders + '.\n' \
-                       + '\n' \
-                         'You have to take care of the pant this week!\n' \
-                         '\n' \
-                         '/Bestyrelsen'}
+    def email_body(self, humanized_bartenders):
+        return f'''Hello {humanized_bartenders}.
 
-        body_text = render_to_string('email.txt', context)
-        body_html = render_to_string('email.html', context)
-        recipient_list = self.filter_with_warning(bartenders)
+You have to take care of pant this week!
 
-        email = EmailMultiAlternatives(
-            subject=subject,
-            body=body_text,
-            from_email='best@fredagscafeen.dk',
-            to=recipient_list,
-            cc=['best@fredagscafeen.dk'],
-            reply_to=['best@fredagscafeen.dk'])
-        email.attach_alternative(body_html, 'text/html')
-        email.send()
-        print('Reminders sent to ' + humanize_bartenders + '!')
+/Bestyrelsen'''
+
+    def email_reply_to(self):
+        return ['best@fredagscafeen.dk']
