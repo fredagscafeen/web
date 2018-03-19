4db5e81780fdde9f5e6b4187d99a6a5a4fb086d6
Tyilo <asgerdrewsen@gmail.com>
2018-03-19 01:52:42 +0100

    Make username field on Bartender unique

--- bartenders/models.py
+++ bartenders/models.py
@@ -14,7 +14,7 @@ from enum import IntEnum
 
 class Bartender(models.Model):
     name = models.CharField(max_length=140)
-    username = models.CharField(max_length=140)
+    username = models.CharField(max_length=140, unique=True)
     email = models.CharField(max_length=255, blank=True)
     studentNumber = models.IntegerField(blank=True, null=True)
     phoneNumber = models.IntegerField(blank=True, null=True)
