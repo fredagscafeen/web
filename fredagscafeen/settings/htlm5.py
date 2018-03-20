54536b9114b7d8332b33688a8660b03d1254340d
Tyilo <asgerdrewsen@gmail.com>
2018-03-20 23:41:23 +0100

    Use new reCAPTCHA key

--- fredagscafeen/settings/htlm5.py
+++ fredagscafeen/settings/htlm5.py
@@ -21,7 +21,7 @@ DATABASES = {'default': dj_database_url.config()}
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-RECAPTCHA_PUBLIC_KEY = '6LeyixoUAAAAAFkkXnzfT8932ENQQmD2a2LLs2Bt'
+RECAPTCHA_PUBLIC_KEY = '6LcM200UAAAAAIi7AHBSlypIewnLk4Q4BvcC8Z-W'
 RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
 
 LOGGING = {
