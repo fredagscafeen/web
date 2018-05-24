a45bc88c0aa601beb58c8e7990b334281d7605be
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2018-05-24 23:52:43 +0200

    Optimize BarTabSnapshotAdmin (DB queries in inline)

--- bartab/admin.py
+++ bartab/admin.py
@@ -1,16 +1,12 @@
-from tempfile import NamedTemporaryFile, TemporaryDirectory
 import subprocess
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
+from admin_views.admin import AdminViews
 from django.contrib import admin
-from django import forms
 from django.forms.widgets import TextInput
 from django.http import FileResponse, HttpResponse
 from django.template.loader import render_to_string
-from django.utils import timezone
-
-from admin_views.admin import AdminViews
-
-from easy_select2 import select2_modelform
+from easy_select2 import Select2
 
 from .models import BarTabUser, BarTabSnapshot, BarTabEntry, SumField
 
@@ -22,11 +18,7 @@ class BarTabUserAdmin(admin.ModelAdmin):
 	list_filter = ('hidden_from_tab',)
 
 
-BarTabEntryForm = select2_modelform(BarTabEntry)
-
-
 class BarTabEntryInline(admin.TabularInline):
-	form = BarTabEntryForm
 	model = BarTabEntry
 	fields = ('raw_added', 'user', 'raw_used')
 	extra = 1
@@ -34,6 +26,23 @@ class BarTabEntryInline(admin.TabularInline):
 	formfield_overrides = {
 		SumField: {'widget': TextInput},
 	}
+
+	def get_queryset(self, request):
+		""" Select related prevents 2*N queries when calling entry.__str__ in each form """
+		return super().get_queryset(request).select_related('user', 'snapshot')
+
+	def formfield_for_foreignkey(self, db_field, request, **kwargs):
+		""" Override user field to cache choices instead of making N queries for BarTabUsers """
+
+		field = super().formfield_for_foreignkey(db_field, request, **kwargs)
+		if db_field.name == 'user':
+			# Here we exploit that choices is a property
+			# On the RHS the original choices function is called which creates a generator that yields choice-tuples from the queryset
+			# The output of the generator is stored in a list to spare each render of the field of evaluating the queryset
+			field.choices = list(field.choices)
+			field.widget = Select2()
+
+		return field
 
 	def formfield_for_dbfield(self, db_field, **kwargs):
 		field = super().formfield_for_dbfield(db_field, **kwargs)
