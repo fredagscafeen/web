2013fa5142e9c9ec64b9b39a0b5ec430b0e4fc32
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-05-27 01:02:12 +0200

    Improve escaping of LaTeX text

--- web/templatetags/latex_trunc.py
+++ web/templatetags/latex_trunc.py
@@ -1,12 +1,37 @@
+import re
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+# From https://stackoverflow.com/questions/16259923/how-can-i-escape-latex-special-characters-inside-django-templates
+def tex_escape(text):
+    """
+        :param text: a plain text message
+        :return: the message escaped to appear correctly in LaTeX
+    """
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless{}',
+        '>': r'\textgreater{}',
+    }
+    return ''.join(conv.get(c, c) for c in text)
+
+
 @register.filter
 @stringfilter
 def latex_trunc(value, width=r'\linewidth'):
-	underscore_escaped_value = value.replace('_', r'\_')
-	#return fr'\truncate{{ {width} }}{{ {underscore_escaped_value} }}'
+	escaped_value = tex_escape(value)
+	#return fr'\truncate{{ {width} }}{{ {escaped_value} }}'
 	# \truncate breaks text too eagerly
-	return fr'\clipbox{{0pt 0pt 0pt 0pt}}{{\parbox{{ {width} }}{{ {underscore_escaped_value} }}}}'
+	return fr'\clipbox{{0pt 0pt 0pt 0pt}}{{\parbox{{ {width} }}{{ {escaped_value} }}}}'
