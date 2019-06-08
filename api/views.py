a3c4df065297aecb396de8d084031c4617f0ab26
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-06-09 01:35:05 +0200

    Remove unused api endpoint

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
@@ -78,17 +77,3 @@ class PrintStatusView(APIView):
 	def get(self, request, job_id):
 		status, code = Printer.get_status(job_id)
 		return Response({'status': status, 'code': code})
-
-
-class UpdateBarcodeView(APIView):
-	permission_classes = (IsAdminUser,)
-
-	def post(self, request):
-		item_id = request.data.get('id')
-		item_barcode = request.data.get('barcode')
-
-		item = get_object_or_404(Item, id=item_id)
-		item.barcode = item_barcode
-		item.save()
-
-		return Response()
