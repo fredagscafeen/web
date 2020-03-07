ed4e916fe9609bacde7d2db10f474921c3248d0d
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-03-08 00:29:35 +0100

    Make response for update_hook better

--- guides/update.py
+++ guides/update.py
@@ -10,9 +10,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 
-BAD_REQUEST = HttpResponseBadRequest('<h1>400 Bad Request</h1>')
-
-
 def get_path(d, *path):
 	for k in path:
 		if not isinstance(d, dict):
@@ -22,40 +19,57 @@ def get_path(d, *path):
 	return d
 
 
+class CalledProcessErrorWithOutput(subprocess.CalledProcessError):
+	def __init__(self, e):
+		self.returncode = e.returncode
+		self.cmd = e.cmd
+		self.output = e.output
+
+	def __str__(self):
+		return super().__str__() + "\nOutput was:\n" + self.output
+
+
+def check_call(*args, **kwargs):
+	try:
+		subprocess.run(*args, **kwargs, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
+	except subprocess.CalledProcessError as e:
+		raise CalledProcessErrorWithOutput(e)
+
+
 @csrf_exempt
 @require_POST
 def update_hook(request):
 	gitlab_token = request.META.get('HTTP_X_GITLAB_TOKEN')
+	if not gitlab_token:
+		return HttpResponseBadRequest('X-Gitlab-Token header is missing')
+
 	gitlab_event = request.META.get('HTTP_X_GITLAB_EVENT')
-	if (not gitlab_token) or gitlab_event != 'Push Hook':
-		return BAD_REQUEST
+	if gitlab_event != 'Push Hook':
+		return HttpResponseBadRequest('X-Gitlab-Event is not "Push Hook"')
 
 	try:
 		data = json.loads(request.body)
 	except json.JSONDecodeError:
-		return BAD_REQUEST
+		return HttpResponseBadRequest('Bad JSON in request body')
 
 	git_url = get_path(data, 'repository', 'git_http_url')
 	if not git_url:
-		return BAD_REQUEST
+		return HttpResponseBadRequest('repository.git_http_url is missing from body')
 
 	url_parts = git_url.split('://', maxsplit=1)
 	if len(url_parts) != 2:
-		return BAD_REQUEST
+		return HttpResponseBadRequest('repository.git_http_url is not a valid url')
 
 	git_url = f'{url_parts[0]}://{gitlab_token}@{url_parts[1]}'
 
 	with TemporaryDirectory() as work_dir:
-		try:
-			subprocess.run(['git', 'clone', git_url, 'repo'], cwd=work_dir, check=True)
+		check_call(['git', 'clone', git_url, 'repo'], cwd=work_dir)
 
-			repo_dir = Path(work_dir) / 'repo'
-			subprocess.run(['./compile'], cwd=repo_dir, check=True)
+		repo_dir = Path(work_dir) / 'repo'
+		check_call(['./compile'], cwd=repo_dir)
 
-			out_dir = repo_dir / 'out'
-			for f in out_dir.iterdir():
-				shutil.copy(f, Path(settings.MEDIA_ROOT) / 'guides')
-		except subprocess.CalledProcessError:
-			return BAD_REQUEST
+		out_dir = repo_dir / 'out'
+		for f in out_dir.iterdir():
+			shutil.copy(f, Path(settings.MEDIA_ROOT) / 'guides')
 
 	return HttpResponse('')
