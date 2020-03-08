f38fe02cc7cef79ab46d394098fdfa41cedde126
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-03-08 01:19:46 +0100

    Run compile after request finishes

--- guides/update.py
+++ guides/update.py
@@ -26,14 +26,24 @@ class CalledProcessErrorWithOutput(subprocess.CalledProcessError):
 		self.output = e.output
 
 	def __str__(self):
-		return super().__str__() + "\nOutput was:\n" + self.output
+		return super().__str__() + "\nOutput was:\n" + str(self.output, 'utf-8', 'backslashreplace')
 
 
 def check_call(*args, **kwargs):
 	try:
-		subprocess.run(*args, **kwargs, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
+		subprocess.run(*args, **kwargs, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	except subprocess.CalledProcessError as e:
 		raise CalledProcessErrorWithOutput(e)
+
+
+class HttpResponseWithJobAfter(HttpResponse):
+	def __init__(self, func, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.func = func
+
+	def close(self):
+		super().close()
+		self.func()
 
 
 @csrf_exempt
@@ -62,14 +72,19 @@ def update_hook(request):
 
 	git_url = f'{url_parts[0]}://{gitlab_token}@{url_parts[1]}'
 
-	with TemporaryDirectory() as work_dir:
-		check_call(['git', 'clone', git_url, 'repo'], cwd=work_dir)
+	work_dir = TemporaryDirectory()
+	check_call(['git', 'clone', git_url, 'repo'], cwd=work_dir.name)
 
-		repo_dir = Path(work_dir) / 'repo'
+	def run_compile():
+		print('Running compile after request...')
+		repo_dir = Path(work_dir.name) / 'repo'
 		check_call(['./compile'], cwd=repo_dir)
 
 		out_dir = repo_dir / 'out'
 		for f in out_dir.iterdir():
 			shutil.copy(f, Path(settings.MEDIA_ROOT) / 'guides')
 
-	return HttpResponse('')
+		work_dir.cleanup()
+		print('Done')
+
+	return HttpResponseWithJobAfter(run_compile, '')
