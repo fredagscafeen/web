88d8b6609c8e0192aa6949e36992ef60327a7922
baltzer77 <kbo0506@gmail.com>
2025-04-15 22:01:07 +0200

    Testing of mail-spotlight

--- reminder/management/commands/send_barshift_reminder.py
+++ reminder/management/commands/send_barshift_reminder.py
@@ -1,5 +1,6 @@
 import datetime
 
+import requests
 from django.utils import timezone
 
 from bartenders.models import BartenderShift, date_format
@@ -20,6 +21,30 @@ class Command(ReminderCommand):
 
     def email_subject(self, humanized_bartenders, event):
         return "Du har en barvagt på fredag!"
+
+    def get_spotlight(self):
+        # Fetch the CSV file from the URL
+        # url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQyLNygOIGu63LRFXuDrQk16sUjzqfAP_6QXllAp5DO3AVVRz9i9_kHPcW20qTthnbOOOVHNxFWf8TT/pub?gid=1786763503&single=true&output=csv"
+        url = "https://script.google.com/macros/s/AKfycbxmPCCxcAJsmH1NTFhme-AWfd6_8FP0D1Ba7Kv0I-b8qwUK_rWx6ktPRt5ydtb4uCQRkA/exec"
+        try:
+            response = requests.get(url)
+            if response.status_code != 200:
+                return ""
+            beers = response.json().get("beer")
+
+            final_text = f"Vi har {len(beers)} øl i spotlight i denne uge:\n\n"
+            for beer in beers:
+                _type = beer.get("type")
+                name = beer.get("name")
+                cost = beer.get("price")
+                notes = beer.get("notes")
+
+                final_text += f"{name}\n - Type: {_type} \n - Smagsnoter: {notes} \n - Pris: {cost:.2f} kr \n\n"
+            print(final_text)
+            return final_text
+        except Exception as e:
+            print(f"Error fetching spotlight data: {e}")
+            return ""
 
     def email_body(self, humanized_bartenders, event):
         start = event.start_datetime - datetime.timedelta(minutes=30)
--- reminder/tests.py
+++ reminder/tests.py
@@ -1,3 +1,17 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .management.commands.send_barshift_reminder import (
+    Command as BarshiftReminderCommand,
+)
+
 
 # Create your tests here.
+class BarshiftReminderTests(TestCase):
+    def setUp(self):
+        self.test_reminder = BarshiftReminderCommand()
+
+    def test_spotlight(self):
+        self.assertEqual(self.test_reminder.get_spotlight(), "Ugens spotlight er:")
