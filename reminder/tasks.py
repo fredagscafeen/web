c967d9225bd18e90645990fb390180372608a302
Anders Bruun Severinsen <202204885@post.au.dk>
2025-06-13 20:36:04 +0200

    todo

--- reminder/tasks.py
+++ reminder/tasks.py
@@ -6,9 +6,6 @@ from .management.commands.send_barshift_reminder import (
 from .management.commands.send_pantvagt_reminder import (
     Command as PantvagtReminderCommand,
 )
-from .management.commands.send_udlejning_reminder import (
-    Command as UdlejningReminderCommand,
-)
 
 
 @shared_task
@@ -20,10 +17,4 @@ def send_barshift_reminder():
 @shared_task
 def send_pantvagt_reminder():
     c = PantvagtReminderCommand()
-    c.run_from_argv(["", ""])
-
-
-@shared_task
-def send_udlejning_reminder():
-    c = UdlejningReminderCommand()
     c.run_from_argv(["", ""])
