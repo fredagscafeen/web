653120e4bf8f5d5d3d7b44c856080196c6554918
Joachim Horshauge <work@joachimhorshauge.com>
2025-10-26 20:39:51 +0100

    Added shelves to admin panel

--- items/models.py
+++ items/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_noop
 
-
 class Item(models.Model):
     brewery = models.ForeignKey(
         "Brewery", on_delete=models.SET_NULL, null=True, blank=True
@@ -20,6 +19,8 @@ class Item(models.Model):
     container_dk = models.CharField(null=True, blank=True, max_length=140)
     volumeInCentiliters = models.IntegerField(null=True, blank=True)
     inStock = models.BooleanField(default=True)
+    glutenFree = models.BooleanField(default=False)
+    nonAlcoholic = models.BooleanField(default=False)
     image = models.ImageField(upload_to="items", blank=True, null=True)
     barcode = models.CharField(max_length=255, unique=True, null=True, blank=True)
 
@@ -41,6 +42,17 @@ class Item(models.Model):
         if not self.barcode:
             self.barcode = None
 
+        # Remove from shelves when going out of stock
+        if self.pk:  # Only for existing items (not new ones)
+            try:
+                old_item = Item.objects.get(pk=self.pk)
+                if old_item.inStock and not self.inStock:
+                    # Import here to avoid circular import
+                    from items.models import ShelfItem
+                    ShelfItem.objects.filter(item=self).delete()
+            except Item.DoesNotExist:
+                pass
+
         super().save(*args, **kwargs)
 
     @property
@@ -51,6 +63,30 @@ class Item(models.Model):
         else:
             return 0
 
+
+class Shelf(models.Model):
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.name
+
+class ShelfItem(models.Model):
+    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE, related_name='shelf_items')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='shelf_items')
+    order = models.PositiveIntegerField(default=0)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('order', 'item__name')
+        unique_together = ('shelf', 'item')
+
+    @property
+    def glutenFree(self):
+        return self.item.glutenFree
+
+    @property
+    def nonAlcoholic(self):
+        return self.item.nonAlcoholic
 
 class BeerType(models.Model):
     name = models.CharField(max_length=140)
