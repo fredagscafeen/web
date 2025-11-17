e1e76a6b40b80a429adc34c53c3fea910f7ad32d
Anders Bruun Severinsen <202204885@post.au.dk>
2025-11-17 16:56:31 +0100

    Update translations and improve footer links; add logo section to about page

--- mail/admin.py
+++ mail/admin.py
@@ -5,5 +5,5 @@ from .models import MailingList
 
 @admin.register(MailingList)
 class MailingListAdmin(admin.ModelAdmin):
-    list_display = ("name", "count")
+    list_display = ("name", "count", "isOnlyInternal")
     filter_horizontal = ("members",)
