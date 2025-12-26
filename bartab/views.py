7a69fcf5af8e61b2dd3ff3ad5a66677de2366d92
Anders Bruun Severinsen <202204885@post.au.dk>
2025-12-26 23:45:31 +0100

    Refine bartab view

--- bartab/views.py
+++ bartab/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.views.generic import DetailView
 
 from .models import BarTabSnapshot, BarTabUser
+
+DEFAULT_ENTRIES_PER_PAGE = 10
 
 
 class BarTab(LoginRequiredMixin, DetailView):
@@ -23,12 +26,35 @@ class BarTab(LoginRequiredMixin, DetailView):
         if not self.object:
             return context
 
-        total_used = 0
+        bartab_entries_pages_per_page = self.request.GET.get(
+            "bartab_entries_pages_per_page"
+        )
+        if (
+            not bartab_entries_pages_per_page
+            or bartab_entries_pages_per_page == "0"
+            or bartab_entries_pages_per_page == ""
+            or not bartab_entries_pages_per_page.isdigit()
+        ):
+            bartab_entries_pages_per_page = DEFAULT_ENTRIES_PER_PAGE
+        context["bartab_entries_pages_per_page"] = bartab_entries_pages_per_page
+
+        paginator_bartab_entries = Paginator(
+            self.object.entries.all(), bartab_entries_pages_per_page
+        )
+
+        bartab_entries_page_number = self.request.GET.get("bartab_entries_page", 1)
+        if not bartab_entries_page_number:
+            bartab_entries_page_number = paginator_bartab_entries.num_pages
+        bartab_entries_page_obj = paginator_bartab_entries.get_page(
+            bartab_entries_page_number
+        )
+
+        context["bartab_entries"] = bartab_entries_page_obj
 
         balance = self.object.balance
-
+        total_used = 0
         context["entries"] = []
-        for entry in self.object.entries.all():
+        for entry in bartab_entries_page_obj:
             shift = entry.snapshot.bartender_shift
 
             context["entries"].append(
